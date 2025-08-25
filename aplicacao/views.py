d1cdaa30b7831fc69525544fa9126b9fb88c779b
dbernardos <121075844+dbernardos@users.noreply.github.com>
2025-08-25 15:38:11 -0300

    desenvolvimento do cadastro de produtos

--- aplicacao/views.py
+++ aplicacao/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Produto
+from django.http.response import HttpResponse
 
 def index(request):
     context = {
@@ -9,7 +10,11 @@ def index(request):
 
 def produto(request):
     produtos = Produto.objects.all()
+    # produtos = get_objects_or_404()
     context = {
         'produtos': produtos,
     }
     return render(request, 'produto.html', context)
+
+def cad_produto(request):
+    return HttpResponse("tela de cadastro de produto")
