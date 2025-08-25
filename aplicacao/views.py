ebe44f644bb10646ada6cd4eb325e27d83e02ffc
dbernardos <121075844+dbernardos@users.noreply.github.com>
2025-08-25 16:14:13 -0300

    cadastro de produto funcionando

--- aplicacao/views.py
+++ aplicacao/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Produto
 from django.http.response import HttpResponse
 
@@ -17,4 +17,18 @@ def produto(request):
     return render(request, 'produto.html', context)
 
 def cad_produto(request):
-    return HttpResponse("tela de cadastro de produto")
+
+    if request.method == "GET":
+        return render(request, 'cad_produto.html')
+    elif request.method == "POST":
+        nome = request.POST.get('nome')
+        preco = request.POST.get('preco').replace(',', '.')
+        qtde = request.POST.get('qtde')
+
+        produto  = Produto(
+            nome = nome,
+            preco = preco,
+            qtde = qtde,
+        )
+        produto.save()
+        return redirect('url_produto')
