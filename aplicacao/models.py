10a2b9d9434f5d309607d39cd310dd2e6dcd5d24
Davi Bernardo Silva <davi.bernardo@cefetsc.edu.br>
2025-09-29 12:35:48 -0300

    correções importacao dados

--- aplicacao/models.py
+++ aplicacao/models.py
@@ -1,5 +1,21 @@
 from django.db import models
 
+class Avaliacao(models.Model):
+    id_evaluation = models.CharField(max_length=255, null=True, blank=True)
+    title = models.CharField(max_length=255, null=True, blank=True)
+    price = models.FloatField(null=True, blank=True)
+    user_id = models.CharField(max_length=100, null=True, blank=True)
+    profile_name = models.CharField(max_length=255, null=True, blank=True)
+    review_helpfulness = models.CharField(max_length=20, null=True, blank=True)
+    review_score= models.FloatField()
+    review_time = models.IntegerField()
+    review_summary = models.CharField(max_length=255, null=True, blank=True)
+    review_text = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.title} - Score: {self.review_score}"
+    
+
 class Produto(models.Model):
     nome = models.CharField("Nome", max_length=200, null = True)
     preco = models.DecimalField("Preço", decimal_places=2, max_digits=8, null = True)
@@ -17,7 +33,7 @@ class Perfil(models.Model):
     cliente = models.OneToOneField(Cliente, on_delete=models.CASCADE, related_name='perfil')
     telefone = models.CharField("Telefone", max_length=20)
     rua = models.CharField("Rua", max_length=200)
-    numero = models.PositiveIntegerField("Nº", max_length=10)
+    numero = models.PositiveIntegerField("Nº")
     cep = models.CharField("CEP", max_length=20)
     bairro = models.CharField("Bairro", max_length=50)
     cidade = models.CharField("Cidade", max_length=50)
